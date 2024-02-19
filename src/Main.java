e4ac77e72e385fe6179d34bf207c822923ac9719
celia115 <celianmolin@gmail.com>
2024-02-19 19:16:52 +0100

    Parametro Peli y Sala creados

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,9 @@ public class Main {
         Cine nombreCine = new Cine("Cinesa", 120);
 
 
+        /**
+         * @param Pelicula Es la clase donde metemos todas las pelis que hay
+         */
         Pelicula pelicula1 = new Pelicula("El Rey León", 90);
         Pelicula pelicula2 = new Pelicula("Enredados", 100);
         Pelicula pelicula3 = new Pelicula("Gru mi villano favorito", 95);
@@ -23,5 +26,24 @@ public class Main {
         Pelicula pelicula8 = new Pelicula("Wonder", 120);
         Pelicula pelicula9 = new Pelicula("Los peores años de mi vida", 92);
         Pelicula pelicula10 = new Pelicula("Culpa mia", 120);
+
+        /**
+         * @param Sala Es la clase donde metemos todas las salas que hay en el cine
+         */
+        Sala sala1 = new Sala(1, pelicula1, 8, 10);
+        Sala sala1_1 = new Sala(2,pelicula2, 8, 10);
+        Sala sala2 = new Sala(2, pelicula3, 13, 6);
+        Sala sala2_1 = new Sala(2,pelicula4, 13, 6);
+        Sala sala3 = new Sala(3, pelicula5, 11, 14);
+        Sala sala3_1 = new Sala(3, pelicula6, 11, 14);
+        Sala sala4 = new Sala(4, pelicula7, 9, 5);
+        Sala sala4_1 = new Sala(4, pelicula8, 9, 5);
+        Sala sala5 = new Sala(5, pelicula9, 8, 20);
+        Sala sala5_1 = new Sala(5, pelicula10, 8, 20);
+
+        nombreCine.agregarSala(sala1);
+        nombreCine.agregarSala(sala2);
+        nombreCine.agregarSala(sala3);
+        nombreCine.agregarSala(sala4);
     }
 }
